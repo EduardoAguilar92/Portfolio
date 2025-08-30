84f78ef6201caa23c25a1b7c48eccdaf03b09d0f
Eduardo Aguilar <lalo.aguilar92@gmail.com>
2025-08-29 22:24:27 -0600

    Update project details

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // Reemplaza 'Portfolio' con el nombre de tu repositorio de GitHub
+  base: '/Portfolio/', 
+});
+
